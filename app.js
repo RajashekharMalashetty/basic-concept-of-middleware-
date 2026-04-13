0691c9cdd3df770c6b36e96fd835286578b8553d
Rajashekhar <malashettyrajashekhar622@gmail.com>
2026-04-13 15:44:14 +0530

    basic concept of middleware

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express()
+
+app.use( (req, res, next) => {
+    console.log("Hi, I am 1st middleware");
+    next()
+});
+
+app.use( (req, res, next) => {
+    console.log("Hi, I am 2st middleware");
+    next()
+});
+app.get("/", (req, res) => {
+    res.send("Hi, I am root.");
+});
+
+app.get("/random", (req, res) => {
+    res.send("this is a random page")
+});
+
+app.listen(8080, () => {
+    console.log("Server listening to port 8080");
+});
